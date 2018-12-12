1a7fab7633067ed83cd80510ac9c56cc110db0c8
Zac <zombiezac@att.net>
2018-12-12 13:56:52 -0700

    init commit

---
+++ WebsiteProject/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebsiteProject
+//
+//  Created by Durham, Zachary on 12/12/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
