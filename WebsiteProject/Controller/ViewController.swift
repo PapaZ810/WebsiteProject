d8d815b5d41c61ff924c09d8da829fe0dd6fb21f
Zac <zombiezac@att.net>
2018-12-14 12:23:46 -0700

    formating

--- WebsiteProject/Controller/ViewController.swift
+++ WebsiteProject/Controller/ViewController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+public class ViewController: UIViewController
+{
+    public override func viewDidLoad() -> Void
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
